7b53e2b55c78c06df90f57d48489acd5cc78ebdf
MrSaharok <111682001+MrSaharok@users.noreply.github.com>
2023-06-01 23:11:26 +0700

    Update route.rb

--- route.rb
+++ route.rb
@@ -1,17 +1,14 @@
+require_relative 'validator'
+
 class Route
   include InstanceCounter
+  include Validation
   attr_reader :stations
 
   def initialize(station_first, station_last)
     @stations = [station_first, station_last]
+    validate!
     register_instance
-    validate!
-  end
-
-  def valid?
-    validate!
-  rescue
-    false
   end
 
   def first_station
@@ -35,7 +32,6 @@ class Route
   protected
 
   def validate(station)
-    # 6 task
     raise "Name station can't be nil !" if station.nil?
     raise "This object: #{station} - is not an instance of the Station class !!!" unless station.class == Station
     raise "Name station can't be Empty !" if station.name.strip.empty?
@@ -45,7 +41,5 @@ class Route
     validate(first_station)
     validate(last_station)
     raise "The route must have different stations! (In this case first: #{first_station.name}, last: #{last_station.name}) " if first_station.name == last_station.name
-    true
   end
-
 end
